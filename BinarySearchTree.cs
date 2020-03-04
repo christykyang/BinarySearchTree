5e7948b1ac5dbbccb53d2fdcb203f455641ae201
unknown <christykyang@gmail.com>
2020-03-04 16:04:10 -0600

    init

---
+++ BinarySearchTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree
+{
+    public class BinarySearchTree
+    {
+        //member variables (HAS A)
+        Node rootNode;
+        bool searchNodeExists;
+
+        //constructor (SPAWNER)
+        public BinarySearchTree()
+        {
+            rootNode = new Node();
+            searchNodeExists = false;
+        }
+
+        //member methods (CAN DO)
+        public void Add(Node node)
+        {
+            Add(node, rootNode);
+        }
+        public void Add(Node node, Node tempNode)
+        {
+            //root null?
+
+            if (node.DataType >= tempNode.DataType)
+            {
+                if (tempNode.rightLink == null)
+                {
+                    tempNode.rightLink = node;
+                    return;
+                }
+                else
+                {
+                    tempNode = tempNode.rightLink;
+                    Add(node, tempNode);
+                }
+                
+            }
+            else //if (node.DataType <= rootNode.DataType)
+            {
+                if (tempNode.leftLink == null)
+                {
+                    tempNode.leftLink = node;
+                    return;
+                }
+                else
+                {
+                    tempNode = tempNode.leftLink;
+                    Add(node, tempNode);
+                }
+            }
+        }
+        public void Search(Node node, Node searchNode)
+        {
+            if (node == searchNode)
+            {
+                searchNodeExists = true;
+            }
+            else if (node.DataType >= searchNode.DataType)
+            {
+                searchNode = searchNode.rightLink;
+                Search(node, searchNode);
+            }
+            else if (node.DataType <= searchNode.DataType)
+            {
+                searchNode = searchNode.leftLink;
+                Search(node, searchNode);
+            }
+            else
+            {
+                searchNodeExists = false;
+            }
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree
+{
+    public class Node
+    {
+        //member variables (HAS A)
+        int dataType;
+        public Node rightLink;
+        public Node leftLink;
+        public int DataType
+        {
+            get
+            { return dataType; }
+        }
+        //constructor (SPAWNER)
+        public Node()
+        {
+            dataType = 0;
+            rightLink = null;
+            leftLink = null;
+        }
+        //member methods (CAN DO)
+    }
+}
